077248feb418df3637130ad004c12eca6c1fd82c
조정현[커머스ID개발] <jeong_hyun.jo@navercorp.com>
2023-12-07 01:58:14 +0900

    Update params.py

--- dags/params.py
+++ dags/params.py
@@ -1,6 +1,6 @@
 import pendulum
 import os
-from dags.utils import batch_utils
+from plugins.utils import batch_utils
 from airflow.decorators import dag, task
 from airflow.models.param import Param
 from airflow.operators.bash import BashOperator

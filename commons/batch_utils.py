dfee71b7e71be5341683e96ed17afbc8c01812c1
조정현[커머스ID개발] <jeong_hyun.jo@navercorp.com>
2023-12-07 03:01:52 +0900

    Update batch_utils.py

--- commons/batch_utils.py
+++ commons/batch_utils.py
@@ -1,7 +1,15 @@
-from commons import env_utils
+# from commons import env_utils
 import urllib.parse
 
 
+
+def get_phase():
+    return "develop"
+
+def is_develop():
+    return get_phase() == "develop"
+
+
 def create_scripts(instance_list, params):
-    batch_scripts = "echo hello, " + env_utils.is_develop()
+    batch_scripts = "echo hello, " + is_develop()
     return batch_scripts
